ce1e885d33e428635e0b96d7eeb3355d1fa59ed9
mariluzpereira <mariluz.pereira@escolar.ifrn.edu.br>
2026-07-13 09:51:02 -0300

    Update jogador.py

--- jogador.py
+++ jogador.py
@@ -3,11 +3,10 @@ from configuracoes import (
     gravidade, cor_safira, cor_louise, cor_anika, cor_meliah
 )
 
-#corrigir o valor da vida do jogador 
 class jogador(pygame.sprite.Sprite):
     def __init__(self, x, y, tipo_personagem):
         super().__init__()
-        self.char_type = tipo_personagem
+        self.tipo_personagem = tipo_personagem
         self.image = pygame.Surface((40, 60))
         
         if tipo_personagem == "Safira": self.color = cor_safira
@@ -18,31 +17,31 @@ class jogador(pygame.sprite.Sprite):
         self.image.fill(self.color)
         self.rect = self.image.get_rect(topleft=(x, y))
         
-        # consertar fisica
+        # Física
         self.vx = 0
         self.vy = 0
-        self.speed = 5
-        self.jump_power = -13
-        self.is_grounded = False
+        self.velocidade = 5
+        self.forca_pulo = -13
+        self.esta_no_chao = False
         
-        # informações do jogador
-        self.max_hearts = 5
-        self.hearts = 5
-        self.stars = 100 
-        self.score = 0
-        self.flowers_collected = 0
+        # Informações do jogador
+        self.coracoes_maximos = 5
+        self.hearts = 5  
+        self.stars = 100  
+        self.score = 0  
+        self.flowers_collected = 0  
         
-        # habilidades/tempo de recarga
-        self.special_active = False
-        self.special_timer = 0
-        self.cooldown_timer = 0    
-        self.max_cooldown = 300    
-        self.invulnerable_timer = 0
+        # Habilidades / Tempo de recarga
+        self.special_active = False  
+        self.tempo_especial = 0
+        self.cooldown_timer = 0  
+        self.tempo_recarga_maximo = 300    
+        self.tempo_invulnerabilidade = 0
 
     def update(self, teclas, plataformas):
-        if self.invulnerable_timer > 0:
-            self.invulnerable_timer -= 1
-            if self.invulnerable_timer % 4 == 0: self.image.set_alpha(100)
+        if self.tempo_invulnerabilidade > 0:
+            self.tempo_invulnerabilidade -= 1
+            if self.tempo_invulnerabilidade % 4 == 0: self.image.set_alpha(100)
             else: self.image.set_alpha(255)
         else:
             self.image.set_alpha(255)
@@ -51,34 +50,37 @@ class jogador(pygame.sprite.Sprite):
             self.cooldown_timer -= 1
 
         self.vx = 0
-        if teclas[pygame.K_a]: self.vx = -self.speed
-        if teclas[pygame.K_d]: self.vx = self.speed
+        if teclas[pygame.K_a]: self.vx = -self.velocidade
+        if teclas[pygame.K_d]: self.vx = self.velocidade
 
-        if self.char_type == "Safira" and self.special_active:
-            self.vy = 1  
-            self.special_timer -= 1
-            if self.special_timer <= 0: 
+        if self.tipo_personagem == "Safira" and self.special_active:
+            self.vy = -2  
+            self.esta_no_chao = False 
+            self.tempo_especial -= 1
+            if self.tempo_especial <= 0: 
                 self.special_active = False
-                self.cooldown_timer = self.max_cooldown 
+                self.cooldown_timer = self.tempo_recarga_maximo 
         else:
             self.vy += gravidade
 
         if self.vy > 15: self.vy = 15
 
-        if (teclas[pygame.K_w] or teclas[pygame.K_SPACE]) and self.is_grounded:
-            self.vy = self.jump_power
-            self.is_grounded = False
+        # Pulo
+        if (teclas[pygame.K_w] or teclas[pygame.K_SPACE]) and self.esta_no_chao:
+            self.vy = self.forca_pulo
+            self.esta_no_chao = False
 
-        if self.special_active and self.char_type != "Safira":
-            self.special_timer -= 1
-            if self.special_timer <= 0: 
+        if self.special_active and self.tipo_personagem != "Safira":
+            self.tempo_especial -= 1
+            if self.tempo_especial <= 0: 
                 self.special_active = False
-                self.cooldown_timer = self.max_cooldown 
+                self.cooldown_timer = self.tempo_recarga_maximo 
 
+        # Movimento e colisão
         self.rect.x += self.vx
         self.tratar_colisao(plataformas, 'x')
         self.rect.y += self.vy
-        self.is_grounded = False
+        self.esta_no_chao = False
         self.tratar_colisao(plataformas, 'y')
 
     def tratar_colisao(self, plataformas, direcao):
@@ -91,7 +93,7 @@ class jogador(pygame.sprite.Sprite):
                     if self.vy > 0:
                         self.rect.bottom = plat.rect.top
                         self.vy = 0
-                        self.is_grounded = True
+                        self.esta_no_chao = True
                     if self.vy < 0:
                         self.rect.top = plat.rect.bottom
                         self.vy = 0
@@ -100,12 +102,28 @@ class jogador(pygame.sprite.Sprite):
         if self.stars >= 30 and not self.special_active and self.cooldown_timer <= 0:
             self.stars -= 30
             self.special_active = True
-            if self.char_type == "Safira": self.special_timer = 180 
-            elif self.char_type == "Louise": self.special_timer = 45  
-            elif self.char_type == "Anika": self.special_timer = 150 
-            elif self.char_type == "Meliah": self.special_timer = 180 
+            if self.tipo_personagem == "Safira": self.tempo_especial = 180   
+            elif self.tipo_personagem == "Louise": self.tempo_especial = 60  
+            elif self.tipo_personagem == "Anika": self.tempo_especial = 150 
+            elif self.tipo_personagem == "Meliah": self.tempo_especial = 180 
 
     def receber_dano(self):
-        if self.invulnerable_timer <= 0:
+        if self.tempo_invulnerabilidade <= 0:
             self.hearts -= 1
-            self.invulnerable_timer = 60
+            self.tempo_invulnerabilidade = 60
+
+    def atacar_inimigos(self, grupo_inimigos):
+        if self.tipo_personagem == "Louise" and self.special_active:
+            area_furacao = self.rect.inflate(100, 40) 
+            
+            for inimigo_alvo in grupo_inimigos:
+                if area_furacao.colliderect(inimigo_alvo.rect):
+                    if hasattr(inimigo_alvo, 'receber_dano'):
+                        inimigo_alvo.receber_dano(dano=1) 
+                    elif hasattr(inimigo_alvo, 'kill'):
+                        inimigo_alvo.kill() 
+
+                    if inimigo_alvo.rect.centerx < self.rect.centerx:
+                        inimigo_alvo.rect.x -= 15  
+                    else:
+                        inimigo_alvo.rect.x += 15
